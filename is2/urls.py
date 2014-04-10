4ddf36e4f7cc4136c87c57bd7f68af6588bcce84
Saul <saul._02@hotmail.com>
2014-04-10 17:49:05 -0400

    Original

--- is2/urls.py
+++ is2/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'is2.views.home', name='home'),
+    url(r'^$', 'zar.views.home'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     # url(r'^blog/', include('blog.urls')),
-
     # url(r'^admin/', include(admin.site.urls)),
 )
--- zar/models.py
+++ zar/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here
 
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(max_length=50)
+    estado = models.IntegerField()
+    password = models.CharField(max_length=128)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    nro_telefono = models.CharField(max_length=13)
+    email = models.CharField(max_length=128)
+    observaciones = models.CharField(max_length=200)
