cd1be4e5918c82a8711d1c92401d1193740faf82
Augusto <agu.amarilla@gmail.com>
2014-04-11 14:04:21 -0400

    Mejora del script 'verDocumentacion.sh'

--- verDocumentacion.sh
+++ verDocumentacion.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Mostrar la documentacion actual
-firefox docs/_build/html/index.html &
+chromium docs/_build/html/index.html &
+if [ $? -z ]; then
+    firefox docs/_build/html/index.html &
+fi
